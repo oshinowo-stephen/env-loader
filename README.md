b86ea0b50d355f370bfd1f5d4a38e3d75d203060
whoastonic <kingstonicthe1@gmail.com>
2021-07-25 01:10:20 -0400

    okay... new process..

--- README.md
+++ README.md
@@ -1,23 +1,9 @@
-# asynv (async-env)
+# docker-secret-loader
 
-`async-env` was already taken on crates so I decided to come up with another troupe. Anyways an asynchronous wrapper around Rust's [`std::env`](https://doc.rust-lang.org/std/env/).
+Loads docker-secrets into the environment written in Rust!
 
-## Usage
+## Example
 
-```rs
-extern crate asynv;
-
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	asynv::set("SOME_VARIABLE", "SOME_VALUE").await;
-
-	let some_var = asynv::get("SOME_VARIABLE").await;
-
-	println!("from environment: {}", some_var); // from environment: SOME_VALUE
-	
-	Ok(())
-}
 ```
-
-## Contribute
-
-If you'll like to see anything, or you have a problem with anything please shoot an [PR]() or [Issue]()
+secrets::load()
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,29 @@
-extern crate async_std;
-
-use std::ffi;
+use std::io::{self, Read};
+use std::fs::{self, File};
 use std::env;
 
-pub async fn rm<K: AsRef<ffi::OsStr>>(k: K)  {
-    env::remove_var(k)
-}
+pub fn load() -> io::Result<()> {
+    let secrets = fs::read_dir("/run/secrets")?;
 
-pub async fn set<K: AsRef<ffi::OsStr>, V: AsRef<ffi::OsStr>>(k: K, v: V) {
-    env::set_var(k, v)
-}
+    for entry in secrets {
+        let secret = entry?;
 
-pub async fn get<K: AsRef<ffi::OsStr>>(k: K) -> Result<String, env::VarError> {
-   env::var(k).map(|val| val)
-}
+        let secret_name = secret.path()
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_uppercase();
 
-#[cfg(test)]
-mod tests {
-    use std::io;
+        let mut secret_content = String::new();
 
-    #[async_std::test]
-    async fn set_env_var() -> io::Result<()> {
-        super::set("TEST_VAR", "TEST_VALUE").await;
+        let mut file = File::open(secret.path())?;
 
-        assert_eq!(&super::get("TEST_VAR").await, "TEST_VALUE");
-
-        Ok(())
-    }
-
-    #[async_std::test]
-    async fn get_env_var() -> io::Result<()> {
-        super::set("TEST_VAR", "SOME_VALUE").await;
-        
-        assert_eq!(&super::get("TEST_VAR").await, "SOME_VALUE");
-
-        Ok(())
-    }
+        file.read_to_string(&mut secret_content)?;
     
-    #[async_std::test]
-    async fn rm_env_var() -> io::Result<()> {
-        super::set("TEST_VAR", "OTHER_VALUE").await;
-
-        super::rm("TEST_VAR").await;
-
-        assert_eq!(super::get("TEST_VAR").await, None);
-
-        Ok(())
+        env::set_var(secret_name, secret_content)
     }
 
-    #[async_std::test]
-    async fn rm_env_after_set() -> io::Result<()> {
-        {
-            super::set("TEST_VAR", "SOME_OTHER_VALUE").await;
-        }
-        
-        super::rm("TEST_VAR").await;
-
-        assert_eq!(super::get("TEST_VAR").await, None);
-
-        Ok(())
-    }
+    Ok(())
 }
+
